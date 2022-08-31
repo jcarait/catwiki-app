0c3fdf2724df39ef5d47f4df7ca56b0c3256768e
Jonathan F-Carait <jonathan.carait@outlook.com>
2022-08-31 19:21:04 +1000

    setup failing test for api image route

--- server/index.test.js
+++ server/index.test.js
@@ -8,10 +8,19 @@ describe("Cat API", () => {
       expect(body.message).toBe("Hello from CatWiki!");
     });
   });
-  describe("get all cat breeds", () => {
+  describe("get all cat breed info", () => {
     describe("given the cat breed info exists", () => {
       it("should return a 200 status and an array of objects", async () => {
         const { body, statusCode } = await request(app).get(`/api/breeds`);
+        expect(statusCode).toBe(200);
+        expect(body).toEqual(expect.arrayContaining([expect.any(Object)]));
+      });
+    });
+  });
+  describe("get all images by cat breed id", () => {
+    describe("given the cat breed info exists", () => {
+      it("should return a 200 status and an array of objects", async () => {
+        const { body, statusCode } = await request(app).get(`/api/images`);
         expect(statusCode).toBe(200);
         expect(body).toEqual(expect.arrayContaining([expect.any(Object)]));
       });
