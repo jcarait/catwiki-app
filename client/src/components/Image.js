cf3eedc0767930b4f5acdbe36db160ef53eaa80c
Jonathan F-Carait <jonathan.carait@outlook.com>
2022-09-01 01:25:10 +1000

    updated image and catprofile component to accept api data props

--- client/src/components/Image.js
+++ client/src/components/Image.js
@@ -2,19 +2,21 @@ import * as React from "react";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 
-export default function Image(props) {
+export default function Image({ images, error }) {
   return (
     <ImageList sx={{}} cols={4} gap={10}>
-      {itemData.map((item) => (
-        <ImageListItem key={item.img}>
-          <img
-            src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-            srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-            alt={item.title}
-            loading="lazy"
-          />
-        </ImageListItem>
-      ))}
+      {images
+        ? images.map((item) => (
+            <ImageListItem key={item.id}>
+              <img
+                src={`${item.url}?w=164&h=164&fit=crop&auto=format`}
+                srcSet={`${item.url}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                alt={item.url}
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))
+        : []}
     </ImageList>
   );
 }
--- client/src/routes/CatProfile.js
+++ client/src/routes/CatProfile.js
@@ -1,14 +1,17 @@
 import { Container } from "@mui/system";
 import { Typography } from "@mui/material";
 import Image from "../components/Image";
+import useFetch from "../utils/useFetch";
 
-export default function CatProfile(props) {
+export default function CatProfile() {
+  const { data, error } = useFetch("http://localhost:3001/api/images/siam");
+
   return (
     <Container>
       <Typography component={"div"} gutterBottom sx={{ color: "black" }}>
-        <h1>{props.name}</h1>
+        <h1>Bengal</h1>
       </Typography>
-      <Image />
+      <Image images={data} error={error} />
     </Container>
   );
 }
